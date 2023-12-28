68146a14512963aa3f72d4ed84e559bff512d88e
VocoPoco <VocoPoco@email.com>
2023-12-28 14:37:07 +0200

    created a task database model

---
+++ src/models/entities/task.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from models.database import Database
+
+db = Database().db
+
+class Task(db.Model):
+    def __init__(self):
+        self.id = self.db.Column(self.db.Integer, primary_key=True)
+        self.title = self.db.Column(self.db.String(120), unique=True, nullable=False)
+        self.description = self.db.Column(self.db.String(500), nullable=True)
+        self.state = self.db.Column(self.db.Enum('TODO', 'In Progress', 'Finished'), default='TODO')
+
+    def __repr__(self):
+        return f"{self.id}, {self.title}, {self.description}, {self.state}" 
